35ed30cf8d6dc6a51bee791902cc087d5058b074
r41l1m4 <rai.lima17@gmail.com>
2024-12-31 09:06:49 -0300

    solved 03_ticket_v1

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,23 +10,40 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+
+    fn is_valid(title: &str, description: &str, status: &str) {
+        Self::is_title_valid(title);
+        Self::is_description_valid(description);
+        Self::is_status_valid(status);
+    }
+
+    fn is_title_valid(title: &str) {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
         if title.len() > 50 {
             panic!("Title cannot be longer than 50 bytes");
         }
+    }
+
+    fn is_description_valid(description: &str) {
         if description.is_empty() {
             panic!("Description cannot be empty");
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
+    }
+
+    fn is_status_valid(status: &str) {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
+    }
 
+
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        Self::is_valid(&title, &description, &status);
         Ticket {
             title,
             description,
@@ -44,6 +61,24 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Self::is_title_valid(&new_title);
+
+        self.title = new_title;
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        Self::is_description_valid(&new_description);
+
+        self.description = new_description;
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Self::is_status_valid(&new_status);
+
+        self.status = new_status;
     }
 }
 
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,76 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    fn is_valid(product_name: &str, quantity: u32, unit_price: u32) {
+        Self::is_product_name_valid(product_name);
+        Self::is_quantity_valid(quantity);
+        Self::is_unit_price_valid(unit_price);
+    }
+
+    fn is_product_name_valid(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty.");
+        }
+        if product_name.len() >= 300 {
+            panic!("Product name can't be longer than 300 bytes.");
+        }
+    }
+
+    fn is_quantity_valid(quantity: u32) {
+        if quantity <= 0 {
+            panic!("Quantity can't be equal or less than 0.");
+        }
+    }
+
+    fn is_unit_price_valid(unit_price: u32) {
+        if unit_price <= 0 {
+            panic!("Unit price can't be equal or less than 0.");
+        }
+    }
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Self::is_valid(&product_name, quantity, unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        &self.quantity * &self.unit_price
+    }
+}
